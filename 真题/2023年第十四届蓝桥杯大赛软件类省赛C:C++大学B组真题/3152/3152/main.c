445b2bff3e808ad5310a3102c0400d21c9f06cc4
Han <kismetfor33455@163.com>
2023-11-21 19:16:01 +0800

    second commit

--- 真题/2023年第十四届蓝桥杯大赛软件类省赛C:C++大学B组真题/3152/3152/main.c
+++ 真题/2023年第十四届蓝桥杯大赛软件类省赛C:C++大学B组真题/3152/3152/main.c
@@ -7,9 +7,9 @@ typedef struct{
     int end;
 } jielong;
 
-int mini_count = 0;
+int mini_delete = 9999;
 int N;
-
+int deleted[100];
 int weishu(int num){
     int ret = 0;
     while (num>10) {
@@ -22,17 +22,34 @@ int weishu(int num){
 int is_jielong(jielong* arr){
     int i = 0;
     for (i=0; i<N-1; i++) {
-        if(arr[i].end != arr[i+1].begin){
+        if(!deleted[i] && arr[i].end != arr[i+1].begin){
             return i;
         }
     }
     return -1;
 }
 
-void delete(jielong* arr){
-    
-    for (int i=0; i<N; i++) {
-        <#statements#>
+void dfs(jielong* a, int n, int delete_count){ //这里用n表示深度
+    if(is_jielong(a) == -1){
+        if(delete_count<mini_delete){
+            mini_delete = delete_count;
+        }
+        return;
+    }
+    if(n == N-1){
+        return;
+    }
+    for (int i=0; i<n; i++) {
+        deleted[i] = 1;
+        delete_count++;
+        if(is_jielong(a) == -1){
+            if(delete_count<mini_delete){
+                mini_delete = delete_count;
+            }
+            return;
+        }
+        dfs(a, n+1, delete_count);
+        deleted[i] = 0;
     }
 }
 
@@ -43,8 +60,10 @@ int main(int argc, const char * argv[]) {
     for (int i=0; i<N; i++) {
         scanf("%d", &arr[i].data);
         arr[i].end = arr[i].data%10;
-        arr[i].begin = arr[i].data/pow(10, weishu(arr[i].data));
+        int a = pow(10, weishu(arr[i].data));
+        arr[i].begin = arr[i].data/a;
     }
-    
+    dfs(arr, 0, 0);
+    printf("%d\n", mini_delete);
     return 0;
 }
